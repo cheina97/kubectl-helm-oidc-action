8604e8b66ffc9d3093fdfa6595bd5c7519dbb08d
Francesco Cheinasso <francesco.cheinasso@staff.aruba.it>
2023-03-20 09:34:35 +0100

    krew + OIDC-login installation in Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,18 @@
 FROM dtzar/helm-kubectl:3.8.0
 
 WORKDIR /app
+ENV PATH="/root/.krew/bin:$PATH"
+
+
+RUN set -x; cd "$(mktemp -d)" && \
+    OS="$(uname | tr '[:upper:]' '[:lower:]')" && \
+    ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" && \
+    KREW="krew-${OS}_${ARCH}" && \
+    curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" && \
+    tar zxvf "${KREW}.tar.gz" && \
+    ./"${KREW}" install krew
+
+RUN $HOME/.krew/bin/kubectl-krew install oidc-login
 
 COPY run.sh .
 
--- run.sh
+++ run.sh
@@ -1,23 +1,19 @@
-#!/bin/sh -eu
+#!/bin/sh
 
-: "${KUBE_CONFIG_DATA?Must be specified}"
+if [ -z "${KUBECONFIG}" ] && [ -z "${KUBE_CONFIG_DATA}" ]; then
+  echo "No KUBECONFIG or KUBE_CONFIG_DATA provided"
+  exit 1
+fi
 
-(
-  set -x; cd "$(mktemp -d)" &&
-  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
-  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
-  KREW="krew-${OS}_${ARCH}" &&
-  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
-  tar zxvf "${KREW}.tar.gz" &&
-  ./"${KREW}" install krew
-)
-
-export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
-
-kubectl krew install oidc-login
 
 # Extract the base64 encoded config data and write this to the KUBECONFIG
-echo "$KUBE_CONFIG_DATA" | base64 -d > /tmp/config
+if [ -n "${KUBECONFIG}" ]; then
+  echo "$KUBECONFIG" | base64 -d > /tmp/config
+fi
+
+if [ -n "${KUBE_CONFIG_DATA}" ]; then
+  echo "$KUBE_CONFIG_DATA" | base64 -d > /tmp/config
+fi
 
 export KUBECONFIG=/tmp/config
 
